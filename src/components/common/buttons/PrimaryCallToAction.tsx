7feab61af3544db23b84c52c0bffa34b1a088d84
Jordan <jwalkercreations@gmail.com>
2020-10-24 17:07:19 -0500

    Updated styling of cta button.

--- src/components/common/buttons/PrimaryCallToAction.tsx
+++ src/components/common/buttons/PrimaryCallToAction.tsx
@@ -14,7 +14,24 @@ type ButtonProps = {
   buttonType?: ButtonTypes;
 };
 
-const StyledPrimaryCallToActionButton = styled.button``;
+const StyledPrimaryCallToActionButton = styled.button`
+  background-color: var(--primary--color-600);
+  color: var(--primary--color-000);
+  border: none;
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: 600;
+  width: 96px;
+  height: 36px;
+  cursor: pointer;
+`;
+
+const StyledButtonArrow = styled.svg`
+  width: 12px;
+  height: 8px;
+  fill: var(--primary--color-100);
+  margin-left: 8px;
+`;
 
 const PrimaryCallToActionButton = ({
   buttonText,
@@ -23,6 +40,9 @@ const PrimaryCallToActionButton = ({
 }: ButtonProps): JSX.Element => (
   <StyledPrimaryCallToActionButton onClick={buttonEvent} {...otherProps}>
     {buttonText}
+    <StyledButtonArrow viewBox="0 0 12 8" xmlns="http://www.w3.org/2000/svg">
+      <path d="M11.3536 4.35355C11.5488 4.15829 11.5488 3.84171 11.3536 3.64645L8.17157 0.464466C7.97631 0.269204 7.65973 0.269204 7.46447 0.464466C7.2692 0.659728 7.2692 0.976311 7.46447 1.17157L10.2929 4L7.46447 6.82843C7.2692 7.02369 7.2692 7.34027 7.46447 7.53553C7.65973 7.7308 7.97631 7.7308 8.17157 7.53553L11.3536 4.35355ZM0 4.5H11V3.5H0V4.5Z" />
+    </StyledButtonArrow>
   </StyledPrimaryCallToActionButton>
 );
 
