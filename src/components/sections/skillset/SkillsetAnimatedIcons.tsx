f620dfe33d91159784d7536d109de7076717fb9b
Jordan <jwalkercreations@gmail.com>
2020-10-24 23:46:44 -0500

    Added animations icons.

--- src/components/sections/skillset/SkillsetAnimatedIcons.tsx
+++ src/components/sections/skillset/SkillsetAnimatedIcons.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { useSpring, animated, config } from "react-spring";
 
 type SkillsetIconProps = {
   iconSources: string[];
   iconAlt: string;
 };
 
-const StyledSkillsetIcon = styled.img``;
+const StyledSkillsetIconOne = styled(animated.img)`
+  position: absolute;
+`;
 
 const StyledIconCircle = styled.div`
   background-color: var(--neutral--color-000);
@@ -18,20 +21,50 @@ const StyledIconCircle = styled.div`
   grid-column: 3 / -1;
   width: 65px;
   height: 65px;
+  position: relative;
+  overflow: hidden;
 `;
 
 const SkillAnimatedIcons = ({
   iconSources,
   iconAlt,
-}: SkillsetIconProps): JSX.Element => (
-  <StyledIconCircle>
-    <StyledSkillsetIcon
-      src={iconSources[1]}
-      alt={iconAlt}
-      width="40px"
-      height="40px"
-    />
-  </StyledIconCircle>
-);
+}: SkillsetIconProps): JSX.Element => {
+  // State for animations
+  const [isHiding, setHiding] = useState(true);
+
+  // Configs for animations
+  const animationPropsOne = useSpring({
+    marginBottom: isHiding ? 150 : 0,
+    delay: 2000,
+    config: config.wobbly,
+    onRest: () => setHiding(!isHiding),
+  });
+
+  const animationPropsTwo = useSpring({
+    marginTop: isHiding ? 0 : 150,
+    delay: 2000,
+    config: config.wobbly,
+    onRest: () => setHiding(!isHiding),
+  });
+
+  return (
+    <StyledIconCircle>
+      <StyledSkillsetIconOne
+        style={animationPropsOne}
+        src={iconSources[0]}
+        alt={iconAlt}
+        width="40px"
+        height="40px"
+      />
+      <StyledSkillsetIconOne
+        style={animationPropsTwo}
+        src={iconSources[1]}
+        alt={iconAlt}
+        width="40px"
+        height="40px"
+      />
+    </StyledIconCircle>
+  );
+};
 
 export default SkillAnimatedIcons;
