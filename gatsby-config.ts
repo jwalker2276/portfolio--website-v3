7788f0c33b006d2329e9223b143388b42b3b42f4
Jordan Walker <jwalkercreations@gmail.com>
2022-06-20 21:21:17 -0500

    Added manifest plugin and static image.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,7 +8,22 @@ const config: GatsbyConfig = {
     siteUrl: `https://www.jordanwalker.dev`,
     image: `/site_main.png`,
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Jordan Walker`,
+        short_name: `Jordan Walker`,
+        start_url: `/`,
+        background_color: `#1f2933`,
+        theme_color: `#1f2933`,
+        display: `standalone`,
+        icon: `src/images/logo_color.svg`,
+      },
+    },
+  ],
 };
 
 export default config;
